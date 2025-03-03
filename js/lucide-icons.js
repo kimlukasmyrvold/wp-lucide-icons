69ab9d807cf6978daeb01b66e933b39f055d6daf
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2025-03-03 13:18:00 +0100

    Re-wrote code, added more error handling and hopefully made script run smoother

--- js/lucide-icons.js
+++ js/lucide-icons.js
@@ -1,127 +1,201 @@
-(function () {
-    if (typeof lucide === 'undefined' || typeof wpLucideIcons === 'undefined') return;
-    
-    function addDrinkToText(icon_name, size, color, stroke_width) {
-        editor.insertContent(`<span style="display:inline-grid">[lucide_icon name="${icon_name}" size="${size}" color="${color}" width="${stroke_width}"]</span> `)
-        document.querySelector('#wp_lucide_icons .wp_lucide_icons__dropdown__content').dataset.lucide_icons_open = 'false'
+'use strict';
+
+function lucideIcons__fillIcons(dropdown) {
+    const defaults = {
+        size: 24,
+        width: 2,
+        color: 'currentColor',
+    };
+
+    const dropdownContent = dropdown.getElementById("wp_lucide_icons__content");
+    if (!dropdownContent) {
+        console.error('Could not find element: "#wp_lucide_icons__content"');
+        return;
     }
-
-    function loadHTML(id, filename) {
-        if (!filename) return;
-        const element = document.querySelector(id);
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    element.innerHTML = this.responseText;
-                    createIconsFromList(element.querySelector('.wp_lucide_icons__dropdown__content__icons'));
-                    lucide.createIcons()
-                    element.querySelectorAll('svg rect').forEach(rect => { // add inline style tag to the recs so it doesn't get overiden by bad css from tinymce
-                        rect.setAttribute('style', `width:${rect.getAttribute('width')}px; height:${rect.getAttribute('height')}px;`)
-                    })
-
-                    const button = element.querySelector('.wp_lucide_icons__dropdown__display__button')
-                    const content = element.querySelector('.wp_lucide_icons__dropdown__content')
-                    const search = element.querySelector('#wp_lucide_icons__search_form')
-                    const clear = element.querySelector('.wp_lucide_icons__dropdown__content__search__input__clear')
-
-                    button.addEventListener('keydown', (e) => { if (e.key === 'Enter' || e.key === 'Space') toggle() })
-                    search.addEventListener('click', (e) => { e.target.focus() })
-                    clear.addEventListener('click', () => { search.value = ''; filterIcons('') })
-                    window.addEventListener('click', handleDropClicks)
-                    window.addEventListener('keydown', handleKeydown)
-
-                    function close() { content.dataset.lucide_icons_open = 'false'; search.blur() }
-                    function open() { content.dataset.lucide_icons_open = 'true'; search.focus() }
-                    function toggle() { content.dataset.lucide_icons_open === 'false' ? open() : close() }
-
-                    function handleKeydown(e) { if (e.key === 'Escape' || e.key === 'Esc') close() }
-                    function handleDropClicks(e) {
-                        if (!button.contains(e.target) && !content.contains(e.target)) close()
-                        if (button.contains(e.target)) toggle()
-                    }
-
-                    // search icons
-                    const icons = Array.from(element.querySelectorAll('.wp_lucide_icons__dropdown__content div[data-icon_name]'))
-                    const iconData = icons.map(icon => ({
-                        element: icon,
-                        name: icon.getAttribute('data-icon_name'),
-                        tags: icon.getAttribute('data-icon_name').split('-'),
-                    }))
-                    const fuse = new Fuse(iconData, {
-                        keys: ['name', 'tags'],
-                        threshold: 0.2,
-                    })
-
-                    function filterIcons(searchQuery) {
-                        const results = fuse.search(searchQuery)
-                        const matchingElements = new Set(results.map(result => result.item.element))
-                        icons.forEach(icon => {
-                            if (searchQuery.length < 1 || matchingElements.has(icon)) icon.style.display = 'initial'
-                            else icon.style.display = 'none'
-                        })
-                    }
-
-                    search.addEventListener('keyup', (e) => {
-                        filterIcons(e.target.value);
-                    })
-                } else if (this.status == 404) { element.innerHTML = '' }
-            };
-        };
-        xhttp.open("GET", `${wpLucideIcons.ajaxUrl}${filename}`, true);
-        xhttp.send();
+    const container = dropdown.getElementById("wp_lucide_icons__icons");
+    if (!container) {
+        console.error('Could not find element: "#wp_lucide_icons__icons"')
         return;
     }
 
-    function createIconsFromList(iconsContainer) {
-        const allIcons = Object.fromEntries(Object.entries(lucide.icons).sort((a, b) => {
-            if (a[0] < b[0]) return -1
-            if (a[0] > b[0]) return 1
-            return 0
-        }))
+    const icons = Object.keys(lucide.icons).map(key => key.replaceAll(/(?<=\w)(?=[A-Z])/g, '-').toLowerCase()).sort((a, b) => a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0);
 
-        for (const icon in allIcons) {
-            const clean_icon = icon.replaceAll(/(?<=\w)(?=[A-Z])/g, '-').toLowerCase()
-
-            const box = document.createElement('div')
-            box.className = 'wp_lucide_icons__dropdown__content__icons__box'
-            box.dataset.icon_name = clean_icon
-
-            const name = document.createElement('span')
-            name.textContent = clean_icon
-
-            const lucideIcon = document.createElement('i')
-            lucideIcon.setAttribute('data-lucide', clean_icon)
-            lucideIcon.setAttribute('width', '24')
-            lucideIcon.setAttribute('height', '24')
-
-            box.addEventListener('click', () => { addDrinkToText(clean_icon, '24', 'currentColor', '2') })
-            box.addEventListener('contextmenu', (e) => {
-                e.preventDefault();
-                navigator.clipboard.writeText(clean_icon);
-            });
-            box.append(name, lucideIcon)
-
-            iconsContainer.append(box);
-        }
+    if (!icons || icons.length < 1) {
+        console.error("Error, something went wrong with getting lucide icons");
+        return;
     }
 
-    if (typeof tinymce === 'undefined') return;
-    tinymce.create('tinymce.plugins.LucideIcons', {
-        init: function (editor) {
-            editor.addButton('lucideicons', {
-                type: 'button',
-                text: '',
-                icon: true,
-                tooltip: 'Insert Lucide Icon',
-                onPostRender: function () {
-                    const div = this.getEl();
-                    div.innerHTML = '<div class="wp_lucide_icons" id="wp_lucide_icons"></div>';
-                    loadHTML('#wp_lucide_icons', 'dropdown.html')
-                },
-            });
-        }
-    });
+    for (const icon of icons) {
+        const box = document.createElement('div');
+        box.className = 'wp_lucide_icons__dropdown__content__icons__box';
+        box.dataset.icon_name = icon;
 
-    tinymce.PluginManager.add('lucideicons', tinymce.plugins.LucideIcons);
-})();
+        const name = document.createElement('span');
+        name.textContent = icon;
+
+        const lucideIcon = document.createElement('i');
+        lucideIcon.setAttribute('data-lucide', icon);
+        lucideIcon.setAttribute('width', defaults.size);
+        lucideIcon.setAttribute('height', defaults.size);
+
+        box.addEventListener('click', () => {
+            if (typeof editor === "undefined") {
+                console.error("Editor is undefined");
+                return;
+            }
+
+            editor.insertContent(`<span style="display:inline-grid">[lucide_icon name="${icon}" size="${defaults.size}" color="${defaults.color}" width="${defaults.width}"]</span> `);
+            dropdownContent.dataset.lucide_icons_open = 'false';
+        });
+        box.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            navigator.clipboard.writeText(icon);
+        });
+
+        box.append(name, lucideIcon);
+        container.append(box);
+    }
+
+    return container;
+}
+
+function lucideIcons__handleDropdown(html) {
+    const container = document.getElementById("wp_lucide_icons");
+    if (!container) {
+        console.error('Could not find element: "#wp_lucide_icons"');
+        return;
+    }
+    const parser = new DOMParser();
+    const dropdown = parser.parseFromString(html, "text/html");
+
+    const iconsContainer = lucideIcons__fillIcons(dropdown);
+    if (!iconsContainer) {
+        console.error('Error filling icons container');
+        return;
+    }
+
+    dropdown.getElementById("wp_lucide_icons__icons").replaceWith(iconsContainer);
+
+    const button = dropdown.querySelector(".wp_lucide_icons__dropdown__display__button");
+    if (!button) {
+        console.error("Could not find dropdown button");
+        return;
+    }
+    const search = dropdown.getElementById("wp_lucide_icons__search_form");
+    if (!search) {
+        console.error("Could not find dropdown search");
+        return;
+    }
+    const searchClear = dropdown.querySelector(".wp_lucide_icons__dropdown__content__search__input__clear");
+    if (!searchClear) {
+        console.error("Could not find dropdown clear search button");
+        return;
+    }
+    const content = dropdown.getElementById("wp_lucide_icons__content");
+    if (!content) {
+        console.error("Could not find dropdown content");
+        return;
+    }
+
+    const contentIcons = content.querySelectorAll('div[data-icon_name]');
+    if (!contentIcons) {
+        console.error("Could not find dropdown content icons");
+        return;
+    }
+
+
+
+    const icons = Array.from(contentIcons);
+    let fuse = null;
+    if (typeof Fuse !== 'undefined') {
+        fuse = new Fuse(icons.map(icon => ({
+            element: icon,
+            name: icon.getAttribute('data-icon_name'),
+            tags: icon.getAttribute('data-icon_name').split('-'),
+        })), {
+            keys: ['name', 'tags'],
+            threshold: 0.2,
+        });
+    }
+
+    function filterIcons(searchQuery) {
+        if (fuse === null) return;
+        const results = fuse.search(searchQuery);
+        const matchingElements = new Set(results.map(result => result.item.element));
+        icons.forEach(icon => { icon.style.display = (searchQuery.length < 1 || matchingElements.has(icon)) ? 'initial' : 'none'; });
+    }
+
+
+    function open() { content.dataset.lucide_icons_open = 'true'; search.focus() }
+    function close() { content.dataset.lucide_icons_open = 'false'; search.blur(); search.value = ''; filterIcons('') }
+    function toggle() { content.dataset.lucide_icons_open === 'false' ? open() : close() }
+    function handleClicks(e) {
+        if (!button.contains(e.target) && !content.contains(e.target)) close();
+        if (button.contains(e.target)) toggle();
+    }
+
+
+    window.addEventListener('click', handleClicks)
+    window.addEventListener('keydown', (e) => { if (e.key === 'Escape' || e.key === 'Esc') close() })
+    button.addEventListener('keydown', (e) => { if (e.key === 'Enter' || e.key === 'Space') toggle() })
+    search.addEventListener('click', (e) => { e.target.focus() })
+    search.addEventListener('keyup', (e) => { filterIcons(e.target.value) })
+    searchClear.addEventListener('click', () => { search.value = ''; filterIcons('') })
+
+
+    container.append(...dropdown.body.childNodes);
+
+    try {
+        lucide.createIcons();
+    } catch (error) {
+        console.error("Could not create lucide icons:", error);
+        return;
+    }
+}
+
+function lucideIcons__createDropdown() {
+    fetch("/wp-content/plugins/wp-lucide-icons/html/dropdown.html")
+        .then(response => response.text())
+        .then(html => {
+            try {
+                lucideIcons__handleDropdown(html);
+            } catch (error) {
+                console.error("Error handling the dropdown:", error);
+                return;
+            }
+        })
+        .catch(error => {
+            console.error('Error loading the HTML file:', error);
+            return;
+        });
+}
+
+function lucideIcons() {
+    if (typeof lucide === 'undefined' || typeof tinymce === 'undefined') return;
+
+    try {
+        tinymce.create('tinymce.plugins.LucideIcons', {
+            init: function (editor) {
+                editor.addButton('lucideicons', {
+                    type: 'button',
+                    text: '',
+                    icon: true,
+                    tooltip: 'Insert Lucide Icon',
+                    onPostRender: function () {
+                        this.getEl().innerHTML = '<div class="wp_lucide_icons" id="wp_lucide_icons"></div>';
+                        lucideIcons__createDropdown();
+                    },
+                });
+            }
+        });
+
+        tinymce.PluginManager.add('lucideicons', tinymce.plugins.LucideIcons);
+    } catch (error) {
+        console.error("Failed to create Lucide dropdown:", error);
+        return;
+    }
+}
+
+if (typeof document !== 'undefined') {
+    document.addEventListener('DOMContentLoaded', lucideIcons);
+}
